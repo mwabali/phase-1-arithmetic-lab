0e2b35c4956959949573915192870058109ec147
Martin Mwabali <martinmwash18952058@gmail.com>
2025-09-30 10:27:55 +0300

    created variables for arithmetic functions

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 22
+let num4 = 6
+let mod = num3 % num4
+let max =Math.max(11, 10,9,8,19,20)
